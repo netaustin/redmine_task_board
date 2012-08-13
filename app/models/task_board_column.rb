f2215e4c98f7c431dfbe3efedd2ba533457a359c
Austin Smith <austin@alleyinteractive.com>
2012-08-13 02:56:55 -0500

    fixing multi-line statements

--- app/models/task_board_column.rb
+++ app/models/task_board_column.rb
@@ -7,8 +7,8 @@ class TaskBoardColumn < ActiveRecord::Base
 
   def self.empty_status(status_id)
     columns = TaskBoardColumn
-      .select(:id)
-      .joins('INNER JOIN issue_statuses_task_board_columns istbc ON istbc.task_board_column_id = task_board_columns.id')
+      .select(:id) \
+      .joins('INNER JOIN issue_statuses_task_board_columns istbc ON istbc.task_board_column_id = task_board_columns.id') \
       .where('istbc.issue_status_id = ?', status_id)
     puts columns
     return columns.empty?
@@ -18,9 +18,9 @@ class TaskBoardColumn < ActiveRecord::Base
   	@column_statuses = Hash.new
   	self.issue_statuses.order(:name).each do |status|
   		@column_statuses[status.id] = Array.new
-  		issues = Issue.select("issues.*, tbi.is_archived, tbi.#{order_column} as weight, tbi.issue_id")
-  			.joins('LEFT OUTER JOIN task_board_issues AS tbi ON tbi.issue_id = issues.id')
-  			.where("project_id = ? AND status_id = ? AND (is_archived IS NULL OR is_archived = 0)", self.project_id, status.id)
+  		issues = Issue.select("issues.*, tbi.is_archived, tbi.#{order_column} as weight, tbi.issue_id") \
+  			.joins('LEFT OUTER JOIN task_board_issues AS tbi ON tbi.issue_id = issues.id') \
+  			.where("project_id = ? AND status_id = ? AND (is_archived IS NULL OR is_archived = 0)", self.project_id, status.id) \
   			.order("weight ASC, created_on ASC")
   		issues.each do |issue|
         # Create a TaskBoardIssue (i.e. a Card) if one doesn't exist already.
