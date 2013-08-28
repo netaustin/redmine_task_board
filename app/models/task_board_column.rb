062f4040b92e4c9ce74e307463a62c6a3f62aeee
Andre Miras <andre.miras@oridao.com>
2013-08-28 16:35:52 +0200

    adds subproject/netsted project support, refs #2

--- app/models/task_board_column.rb
+++ app/models/task_board_column.rb
@@ -16,11 +16,12 @@ class TaskBoardColumn < ActiveRecord::Base
 
   def issues(order_column="project_weight")
   	@column_statuses = Hash.new
+        subproject_ids = project.self_and_descendants.collect {|p| p.id}.flatten
   	self.issue_statuses.order(:name).each do |status|
   		@column_statuses[status.id] = Array.new
   		issues = Issue.select("issues.*, tbi.is_archived, tbi.#{order_column} as weight, tbi.issue_id") \
   			.joins('LEFT OUTER JOIN task_board_issues AS tbi ON tbi.issue_id = issues.id') \
-  			.where("project_id = ? AND status_id = ? AND (is_archived IS NULL OR is_archived = 0)", self.project_id, status.id) \
+  			.where("project_id IN (?) AND status_id = ? AND (is_archived IS NULL OR is_archived = FALSE)", subproject_ids, status.id) \
   			.order("weight ASC, created_on ASC")
   		issues.each do |issue|
         # Create a TaskBoardIssue (i.e. a Card) if one doesn't exist already.
